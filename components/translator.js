d05dfa02b838b5d13f8b4f936089afc6f538c60a
Abdelrahman <said.abdelrahman89@gmail.com>
2021-06-02 02:21:02 +0100

    Started implementing the logic for the translation function

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,236 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanPhrases = require("./american-only.js");
+
+// Make sure all phrases are lowercase
+const americanOnly = Object.entries(americanPhrases).reduce(
+    (obj, cur) => ({ ...obj, [cur[0].toLowerCase()]: cur[1].toLowerCase() }),
+    {}
+);
+
+const britishPhrases = require("./british-only.js");
+
+// Make sure all phrases are lowercase
+const britishOnly = Object.entries(britishPhrases).reduce(
+    (obj, cur) => ({ ...obj, [cur[0].toLowerCase()]: cur[1].toLowerCase() }),
+    {}
+);
+
+const spelling = require("./american-to-british-spelling.js");
+
+// Make sure all spelling phrases are lowercase
+const americanToBritishSpelling = Object.entries(spelling).reduce(
+    (obj, cur) => ({ ...obj, [cur[0].toLowerCase()]: cur[1].toLowerCase() }),
+    {}
+);
+// Swap the objects keys and values to make searching for British phrases easier
+const britishToAmericanSpelling = Object.entries(
+    americanToBritishSpelling
+).reduce((obj, cur) => ({ ...obj, [cur[1]]: cur[0] }), {});
+
+const titles = require("./american-to-british-titles.js");
+
+// Make sure all titles are lowercase
+const americanToBritishTitles = Object.entries(titles).reduce(
+    (obj, cur) => ({ ...obj, [cur[0].toLowerCase()]: cur[1].toLowerCase() }),
+    {}
+);
+// Swap the objects keys and values to make searching for British phrases easier
+const britishToAmericanTitles = Object.entries(americanToBritishTitles).reduce(
+    (obj, cur) => ({ ...obj, [cur[1]]: cur[0] }),
+    {}
+);
+
+// HELPER FUNCTIONS
+function splitString(str, length) {
+    /*
+     * Takes a string and a number for length. Length defines how the string is
+     * going to be split. If the length is less than 1, then it returns an empty
+     * array. If the length equals 1, it will just split the string into words.
+     * If the length is greater than 1, then the function will split the string
+     * into phrases that each has an n number of words where n equals length. It
+     * achieves that by concatenating as many previous words as possible to the
+     * current word. If there are not enough words to match the length, then it
+     * just returns what is available.
+     */
+    if (length < 1) {
+        return [];
+    } else if (length === 1) {
+        return str.split(" ");
+    } else {
+        const splitSentence = str.split(" ");
+
+        return splitSentence.map((word, idx) => {
+            /*
+             * Get the start index where the splitSentence array will be sliced.
+             * This is done by going backwards from the current index by the
+             * length amount plus 1. We add 1 here, so the final length of the
+             * phrase will be equal to the length specified as an argument. If we
+             * don't add 1, the length of the phrase will be 1 word longer than
+             * the specified length.
+             *
+             * EXAMPLE:
+             * Take the sentence: I want to learn programming.
+             * Let's say we want to split it into phrases that are each 2 words long.
+             * If we are currently at the word 'learn', then the current index is 3.
+             * If we subtract 2, then the start index would be 1.
+             * When we use the function slice(start, index), this would be slice(1, 3),
+             * which will give us the words 'want to'.
+             * When concatenated to 'learn', the resultant phrase would be 'want to learn',
+             * which is 3 words long rather than 2.
+             */
+            let start = idx - length + 1;
+
+            while (start < 0) {
+                ++start;
+            }
+
+            return [...splitSentence.slice(start, idx), word].join(" ");
+        });
+    }
+}
+
+function getVaryingLengthPhrases(str) {
+    const oneWord = str.split(" ");
+    const twoWord = splitString(str, 2);
+    const threeWord = splitString(str, 3);
+    const fourWord = splitString(str, 4);
+    const fiveWord = splitString(str, 5);
+
+    return [...oneWord, ...twoWord, ...threeWord, ...fourWord, ...fiveWord];
+}
+
+function getDictionary(locale) {
+    if (locale === "american-to-british") {
+        return [
+            americanToBritishTitles,
+            americanOnly,
+            americanToBritishSpelling,
+        ];
+    } else if (locale === "british-to-american") {
+        return [
+            britishToAmericanTitles,
+            britishOnly,
+            britishToAmericanSpelling,
+        ];
+    }
+}
+
+function translateNonTitles(phrase, dict) {
+    const punctuation = [".", ",", ";", "?", "!"];
+    const lastChar = phrase[phrase.length - 1];
+
+    let appendChar = false;
+
+    if (punctuation.includes(lastChar)) {
+        phrase = phrase.slice(0, phrase.length - 1);
+
+        appendChar = true;
+    }
+
+    const translation = dict[phrase];
+
+    if (translation && appendChar) {
+        return translation + lastChar;
+    } else if (translation) {
+        return translation;
+    }
+}
+
+function translateTime(sentence, locale) {
+    let output = {
+        matches: [],
+        indices: [],
+        lengths: [],
+        translation: sentence,
+    };
+
+    if (locale === "american-to-british") {
+        const regex = /(\d{1,2}):(\d{1,2})/g;
+        output.translation = output.translation.replace(
+            regex,
+            (match, p1, p2, offset) => {
+                output.matches.push(match);
+                output.indices.push(offset);
+                output.lengths.push(match.length);
+                return `${p1}.${p2}`;
+            }
+        );
+    } else if (locale === "british-to-american") {
+        const regex = /(\d{1,2}).(\d{1,2})/g;
+        output.translation = output.translation.replace(
+            regex,
+            (match, p1, p2, offset) => {
+                output.matches.push(match);
+                output.indices.push(offset);
+                output.lengths.push(match.length);
+                return `${p1}:${p2}`;
+            }
+        );
+    }
+
+    return output;
+}
+
+function matchCase(translationObj) {}
 
 class Translator {
+    translate(string, locale) {
+        let output = translateTime(string, locale);
+        const originalSentence = output.translation;
+        output.translation = output.translation.toLowerCase();
 
+        const phrases = getVaryingLengthPhrases(originalSentence).sort(
+            (a, b) => b.length - a.length
+        );
+
+        const dictionary = getDictionary(locale);
+        const titles = dictionary[0];
+        const wordsAndIdioms = dictionary.slice(1);
+
+        for (let phrase of phrases) {
+            phrase = phrase.toLowerCase();
+
+            let replacement;
+
+            if (titles[phrase]) {
+                replacement = titles[phrase];
+            } else {
+                for (let dict of wordsAndIdioms) {
+                    const translation = translateNonTitles(phrase, dict);
+                    if (translation) {
+                        replacement = translation;
+                    }
+                }
+            }
+
+            if (replacement) {
+                const match = output.translation.match(phrase);
+
+                if (match) {
+                    output.matches.push(
+                        originalSentence.slice(
+                            match.index,
+                            match.index + phrase.length
+                        )
+                    );
+
+                    output.lengths.push(replacement.length);
+
+                    output.translation = output.translation.replace(
+                        phrase,
+                        replacement
+                    );
+
+                    output.indices.push(
+                        output.translation.match(replacement).index
+                    );
+                }
+            }
+        }
+
+        // TODO: figure out the matchCase function and try to improve the translation function
+        return output;
+    }
 }
 
 module.exports = Translator;
+
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,191 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite('Unit Tests', () => {
+suite("Unit Tests", function () {
+    suite("Translator", function () {
+        suite("translate", function () {
+            test("Translate Mangoes are my favorite fruit. to British English", function () {
+                const translationObj = translator.translate(
+                    "Mangoes are my favorite fruit.",
+                    "american-to-british"
+                );
 
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "Mangoes are my favourite fruit."
+                );
+            });
+
+            test("Translate I ate yogurt for breakfast. to British English", function () {
+                const translationObj = translator.translate(
+                    "I ate yogurt for breakfast.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "I ate yoghurt for breakfast."
+                );
+            });
+
+            test("Translate We had a party at my friend's condo. to British English", function () {
+                const translationObj = translator.translate(
+                    "We had a party at my friend's condo.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "We had a party at my friend's flat."
+                );
+            });
+
+            test("Translate Can you toss this in the trashcan for me? to British English", function () {
+                const translationObj = translator.translate(
+                    "Can you toss this in the trashcan for me?",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "Can you toss this in the bin for me?"
+                );
+            });
+
+            test("Translate The parking lot was full. to British English", function () {
+                const translationObj = translator.translate(
+                    "The parking lot was full.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "The car park was full."
+                );
+            });
+
+            test("Translate Like a high tech Rube Goldberg machine. to British English", function () {
+                const translationObj = translator.translate(
+                    "Like a high tech Rube Goldberg machine.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "Like a high tech Heath Robinson device."
+                );
+            });
+
+            test("Translate To play hooky means to skip class or work. to British English", function () {
+                const translationObj = translator.translate(
+                    "To play hooky means to skip class or work.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "To bunk off means to skip class or work."
+                );
+            });
+
+            test("Translate No Mr. Bond, I expect you to die. to British English", function () {
+                const translationObj = translator.translate(
+                    "No Mr. Bond, I expect you to die.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "No Mr Bond, I expect you to die."
+                );
+            });
+
+            test("Translate Dr. Grosh will see you now. to British English", function () {
+                const translationObj = translator.translate(
+                    "Dr. Grosh will see you now.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "Dr Grosh will see you now."
+                );
+            });
+
+            test("Translate Lunch is at 12:15 today. to British English", function () {
+                const translationObj = translator.translate(
+                    "Lunch is at 12:15 today.",
+                    "american-to-british"
+                );
+
+                assert.isNotEmpty(translationObj);
+                assert.containsAllKeys(translationObj, [
+                    "indices",
+                    "lengths",
+                    "translation",
+                ]);
+                assert.equal(
+                    translationObj.translation,
+                    "Lunch is at 12.15 today."
+                );
+            });
+        });
+    });
 });
